ce91d9df3b400fc5eea357207a61c6807a64e04b
gaonkarswapnil <31978392+gaonkarswapnil@users.noreply.github.com>
2023-11-30 15:50:08 +0530

    Hello Servlet commit

---
+++ src/main/java/day3/HelloWorldServlet.java
@@ -0,0 +1,37 @@
+package day3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String mimeType = "text/html";
+		response.setContentType(mimeType);
+		
+		String responseText = "<h1 style='color: red'>Hello World from Servlet</h1>";
+		PrintWriter out = response.getWriter();
+		out.print(responseText);
+	}
+
+}
